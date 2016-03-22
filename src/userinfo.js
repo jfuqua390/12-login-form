8296a65a423a5672bd6c75a62761e2f4814903a9
Jonathan Fuqua <jfuqua390@gmail.com>
2016-03-22 17:09:57 -0500

    Deleted a superfluous .js file

--- src/userinfo.js
+++
@@ -1,7 +0,0 @@
-
-export default userinfo [
-  { username: `aaron@theironyard.com`, password: `password123` },
-  { username: `admin@google.com`, password: `pandas` },
-  { username: `email@email.com`, password: `honeycrisp` },
-
-];
