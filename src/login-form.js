3760d6a15f0fbd4caf0f3461accc5d22d39e2761
Jonathan Fuqua <jfuqua390@gmail.com>
2016-03-22 17:04:56 -0500

    Complete first test

--- src/login-form.js
+++ src/login-form.js
@@ -1,2 +1,22 @@
 
-export default undefined;
+
+
+export default class LoginForm {
+  constructor(form, email) {
+    this.userinfo = [
+      { username: `aaron@theironyard.com`, password: `password123` },
+      { username: `admin@google.com`, password: `pandas` },
+      { username: email, password: `honeycrisp` },
+    ];
+
+  }
+  validate(username, password) {
+    return this.userinfo.reduce((prev, current) => {
+      if (current.username === username && current.password === password) {
+        return true;
+      }
+
+      return prev;
+    }, false);
+  }
+}
---
+++ src/userinfo.js
@@ -0,0 +1,7 @@
+
+export default userinfo [
+  { username: `aaron@theironyard.com`, password: `password123` },
+  { username: `admin@google.com`, password: `pandas` },
+  { username: `email@email.com`, password: `honeycrisp` },
+
+];
